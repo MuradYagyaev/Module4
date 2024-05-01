6daf70b36fc7bd18d1b36ae5ad494a4ca886911b
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-05-01 22:44:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# Модуль 4 урок №1. "Модули и пакеты"
+
+# Вариант 1
+# import homework1
+# homework1.func1()
+# homework1.func2()
+
+# Вариант 2
+# import homework1 as hw1
+# hw1.func1()
+# hw1.func2()
+
+# Вариант 3
+# from homework1 import func1, func2
+# func1()
+# func2()
+
+# Вариант 4
+# from homework1 import *
+# func1()
+# func2()
+
+# Вариант 5
+from homework1 import func1 as f1, func2 as f2
+f1()
+f2()
